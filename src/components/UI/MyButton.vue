8eb09b4e2ef390111f20d1e3ecbeecf3ad93faf9
Ksusha <xiuschv@yandex.ru>
2023-07-22 10:46:34 +0300

    добавлен компонент кнопки

---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="btn">
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.btn {
+  background: none;
+  border: 1px solid hotpink;
+  color: hotpink;
+  padding: 1rem;
+  margin: 1rem;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.btn:hover {
+  background: hotpink;
+  color: white;
+}
+</style>
