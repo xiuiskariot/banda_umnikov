c0d1782b8df28e5b9006abaad7fb28bdbea2c870
Ksusha <xiuschv@yandex.ru>
2023-07-23 21:57:54 +0300

    fix deploy

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  pwa: {
+    workboxOptions: {
+      exclude: [/_redirects/],
+    },
+  },
+};
